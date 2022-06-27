0d930355f7ac5280955879ed9103d3a8b1a5a7b3
Eslam Elshaabany <Elshaabany@protonmail.com>
2022-06-27 14:45:27 +0200

    handel /hello request

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 
+// respond to requests for /hello
+app.get("/hello", (req, res) => {
+    res.send("Hello World!");
+});
+
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
