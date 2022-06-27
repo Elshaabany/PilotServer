cc5dd1764592bcecc85ce426a1a11a7444e5846f
Eslam Elshaabany <Elshaabany@protonmail.com>
2022-06-27 14:36:54 +0200

    Express server using copilot

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+}
